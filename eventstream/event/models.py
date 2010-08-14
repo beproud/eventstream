a955c15144152d8c0b67d82110cf7a5c830312f5
ae35 <ae35@beproud.jp>
2010-08-14 17:13:45 +0900

    #3 ADD: Event model

--- eventstream/event/models.py
+++ eventstream/event/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class EventManager(models.Manager):
+    """Event model manager
+    """
+    def by_account(self, account):
+        """Return event query-set related with the account.
+        """
+        return self.filter(user=account)
+
+class Event(models.Model):
+    """Event model
+    """
+    object = EventManager()
+
+    user = models.ForeignKey('account.Account')
+    name = models.CharField('Title', max_length=255)
+    description = models.TextField('Event Description', max_length=1000, blank=True)
+    body = models.TextField('Body Text', max_length=1000)
+    member_limit = models.IntegerField('Member Limit', blank=True, null=True)
+    place = models.CharField('Place', max_length=255, blank=True, null=True)
+    address = models.CharField('Address', max_length=255, blank=True, null=True)
+    hashtag = models.CharField('Twitter Hashtag', max_length=100, blank=True, null=True)
+    started_at = models.DateTimeField('Start Time')
+    ended_at = models.DateTimeField('End TIme')
+
+    class Meta:
+        db_table = 'event'
+
+    def __unicode__(self):
+        return u'%s: %s' % (self.id, self.name)
+
