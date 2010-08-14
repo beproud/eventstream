d37fa31d4be9aeb304cf28a412646265836dae1f
ae35 <ae35@beproud.jp>
2010-08-14 17:42:01 +0900

    #3 add event test

--- eventstream/event/models.py
+++ eventstream/event/models.py
@@ -7,7 +7,7 @@ from account.models import Account
 
 
 class EventManager(models.Manager):
-    """
+    """イベントマネージャ
     """
     def by_account(self, account):
         """指定アカウント主催のイベント取得
@@ -17,16 +17,16 @@ class EventManager(models.Manager):
 class Event(models.Model):
     """イベントモデル
     """
-    object = EventManager()
+    objects = EventManager()
 
     user = models.ForeignKey(Account)
-    name = models.CharField(u'イベントタイトル', max_length=255)
+    name = models.CharField(u'タイトル', max_length=255)
     description = models.TextField(u'説明', max_length=1000, blank=True)
     body = models.TextField(u'内容', max_length=1000)
     member_limit = models.IntegerField(u'定員', blank=True, null=True)
     place = models.CharField(u'会場', max_length=255, blank=True, null=True)
     address = models.CharField(u'住所', max_length=255, blank=True, null=True)
-    hashtag = models.CharField(u'Twitter ハッシュタグ', max_length=100, blank=True, null=True)
+    hashtag = models.CharField(u'Twitterハッシュタグ', max_length=100, blank=True, null=True)
     started_at = models.DateTimeField(u'開始日時', default=datetime.now)
     ended_at = models.DateTimeField(u'終了日時', default=datetime.now)
 
