ae2182eb522be06089058a0204e028ba086f74a8
Shinya Okano <tokibito@beproud.jp>
2010-08-14 21:33:28 +0900

    #19 account情報をDBから取得してrequestに付与する

--- eventstream/account/middleware.py
+++ eventstream/account/middleware.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from account.utils import has_account, ACCOUNT_SESSION_KEY
+from account.models import Account
 
 class AccountMiddleware(object):
     def process_request(self, request):
         """
         セッションのアカウント情報をrequestに付与する
         アカウント情報がない場合はaccountにNoneを入れる
+        request.authinfoがある場合はDBにアカウントを問い合わせる
 
           if request.account:
               # アカウント有
@@ -17,4 +19,7 @@ class AccountMiddleware(object):
         if has_account(request):
             request.account = request.session.get(ACCOUNT_SESSION_KEY)
         else:
-            request.account = None
+            if getattr(request, 'authinfo', None):
+                request.account = Account.objects.get_by_openid_url(openid_url=request.authinfo.url, return_none=True)
+            else:
+                request.account = None
