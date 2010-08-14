cf2ce36e501a4af8b70ab9b4488d3558bbb62fab
Shinya Okano <tokibito@beproud.jp>
2010-08-14 16:32:28 +0900

    #5 Accountモデルを追加

--- eventstream/account/models.py
+++ eventstream/account/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class AccountManager(models.Manager):
+    pass
+
+class Account(models.Model):
+    """
+    サイトのユーザアカウントモデル
+    openid_urlは一意になるはず
+    """
+    user = models.ForeignKey(User, unique=True)
+    openid_url = models.URLField(u'OpenID', verify_exists=False, unique=True) # OpenID認証用
+    name = models.CharField(u'アカウント名', max_length=20)
+    url = models.URLField(u'URL', verify_exists=False, null=True, blank=True)
+    twitter_account = models.CharField(u'Twitter', max_length=20)
+    created_at = models.DateTimeField(u'作成日時', default=datetime.now)
+    notify_email = models.BooleanField(u'メールによる通知', default=True)
+
+    objects = AccountManager()
+
+    class Meta:
+        db_table = 'account'
+        verbose_name = verbose_name_plural = u'アカウント'
