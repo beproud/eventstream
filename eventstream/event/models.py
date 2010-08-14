bfc885a3691577c518dacaac7f3d34ecb469439f
ae35 <ae35@beproud.jp>
2010-08-14 17:25:04 +0900

    #3 タイトルなどを日本語に

--- eventstream/event/models.py
+++ eventstream/event/models.py
@@ -1,32 +1,38 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
 from django.db import models
 
+from account.models import Account
+
+
 class EventManager(models.Manager):
-    """Event model manager
+    """
     """
     def by_account(self, account):
-        """Return event query-set related with the account.
+        """指定アカウント主催のイベント取得
         """
         return self.filter(user=account)
 
 class Event(models.Model):
-    """Event model
+    """イベントモデル
     """
     object = EventManager()
 
-    user = models.ForeignKey('account.Account')
-    name = models.CharField('Title', max_length=255)
-    description = models.TextField('Event Description', max_length=1000, blank=True)
-    body = models.TextField('Body Text', max_length=1000)
-    member_limit = models.IntegerField('Member Limit', blank=True, null=True)
-    place = models.CharField('Place', max_length=255, blank=True, null=True)
-    address = models.CharField('Address', max_length=255, blank=True, null=True)
-    hashtag = models.CharField('Twitter Hashtag', max_length=100, blank=True, null=True)
-    started_at = models.DateTimeField('Start Time')
-    ended_at = models.DateTimeField('End TIme')
+    user = models.ForeignKey(Account)
+    name = models.CharField(u'イベントタイトル', max_length=255)
+    description = models.TextField(u'説明', max_length=1000, blank=True)
+    body = models.TextField(u'内容', max_length=1000)
+    member_limit = models.IntegerField(u'定員', blank=True, null=True)
+    place = models.CharField(u'会場', max_length=255, blank=True, null=True)
+    address = models.CharField(u'住所', max_length=255, blank=True, null=True)
+    hashtag = models.CharField(u'Twitter ハッシュタグ', max_length=100, blank=True, null=True)
+    started_at = models.DateTimeField(u'開始日時', default=datetime.now)
+    ended_at = models.DateTimeField(u'終了日時', default=datetime.now)
 
     class Meta:
         db_table = 'event'
+        verbose_name = verbose_name_plural = u'イベント'
 
     def __unicode__(self):
         return u'%s: %s' % (self.id, self.name)
-
