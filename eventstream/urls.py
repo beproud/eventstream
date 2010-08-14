ffd219c17df477fec181bf3330c80db1727324e0
Shinya Okano <tokibito@beproud.jp>
2010-08-14 15:28:44 +0900

    #14 静的ファイルの設定

--- eventstream/urls.py
+++ eventstream/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 
 from django.contrib import admin
@@ -6,3 +7,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
