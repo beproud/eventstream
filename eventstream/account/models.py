6900877f258e4bda6feab22d8b11563b5609569d
Shinya Okano <xxshss@yahoo.co.jp>
2010-08-15 10:11:06 +0900

    #21 アカウントからnicknameを削除

--- eventstream/account/models.py
+++ eventstream/account/models.py
@@ -5,13 +5,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class AccountManager(models.Manager):
-    def create_user_account(self, username, email, password, openid_url, nickname, commit=True):
+    def create_user_account(self, username, email, password, openid_url, commit=True):
         """
         UserとAccountをまとめて作成
         commit=Falseとしてもuserは作成される
         """
         user = User.objects.create_user(username, email, password)
-        account = self.model(user=user, openid_url=openid_url, nickname=nickname)
+        account = self.model(user=user, openid_url=openid_url)
         if commit:
             account.save()
         return account
@@ -33,7 +33,6 @@ class Account(models.Model):
     """
     user = models.ForeignKey(User, unique=True)
     openid_url = models.URLField(u'OpenID', verify_exists=False, unique=True) # OpenID認証用
-    nickname = models.CharField(u'ニックネーム', max_length=20)
     url = models.URLField(u'URL', verify_exists=False, null=True, blank=True)
     twitter_account = models.CharField(u'Twitter', max_length=20, null=True, blank=True)
     created_at = models.DateTimeField(u'作成日時', default=datetime.now)
