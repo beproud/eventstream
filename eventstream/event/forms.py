006240d2ab9018b10cae731981dc80eeb5ef4f50
Yosuke "AE35" IKEDA <yosuke.ikeda@beproud.jp>
2010-08-15 17:41:17 +0900

    #35 開始日時 < 終了日時 チェックを追加

--- eventstream/event/forms.py
+++ eventstream/event/forms.py
@@ -11,6 +11,15 @@ __all__ = (
 class EventForm(forms.ModelForm):
     """イベント登録フォーム
     """
+    def clean_ended_at(self):
+        """開始日時 < 終了日時バリデート
+        """
+        st = self.cleaned_data['started_at']
+        ed = self.cleaned_data['ended_at']
+        if st >= ed:
+            raise forms.ValidationError(u'終了日時は開始日時以降に設定して下さい。')
+        return ed
+
     class Meta:
         model = Event
         exclude = ('user','participants')
